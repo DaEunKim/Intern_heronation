195960ff6a0efe168aae12378209228475c8d06d
DaEunKim <ekdms717@naver.com>
2017-12-19 14:32:45 +0900

    1. html에서 입력 받은 데이터를 php로 넘기기 2. php로 들어온 입력을 데이터베이스 쿼리로 넘겨주기 3. DB를 읽어서 html로 데이터 보내기(location.href 사용)

---
+++ test.php
@@ -0,0 +1,32 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "0505";
+$dbname = "test";
+$tablename = "dani_test";
+
+// Create connection
+$conn =mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$db_index = (int)($_GET['index']);
+$db_companyID = (int)($_GET['companyID']);
+$db_countryID = (int)($_GET['countryID']);
+$db_ID = htmlspecialchars($_GET['ID']);
+$db_PW = htmlspecialchars($_GET['PW']);
+
+$sql = "SELECT * FROM dani_test";
+$result = $conn->query($sql);
+$insertsql = "INSERT INTO $tablename VALUES($db_index, $db_companyID, $db_countryID, '$db_ID', '$db_PW')";
+if(mysqli_query($conn, $insertsql)){
+   echo "<script>location.href='http://heronation.net/test/dani/dani.html?index=" . $db_index . "&companyID=" . $db_companyID .
+   "&countryID=" . $db_countryID . "&ID=" . $db_ID . "&PW=" . $db_PW . "';</script>";
+   echo "success";
+}
+
+$conn->close();
+
+?>
