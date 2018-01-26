9f7907f520dfe25c80bfb76fa8381b742ec4ffa3
DaEunKim <ekdms717@naver.com>
2018-01-26 17:44:05 +0900

    making sorting method

--- sizeinfophp/searchproductlist.php
+++ sizeinfophp/searchproductlist.php
@@ -11,11 +11,14 @@ if(isset($_COOKIE["userPKeyZeyo"])){
 // left join User on User.PKey = Product.UserID
 // left join Category on Product.CategoryID = Category.PKey
 // where Product.UserID = '.$userID.' ';
-$productListQuery = 'SELECT Product.PKey as ProductPKey, Product.CreatedDate as ProductCreatedDate,
-Category.NameKR as CategoryName, Product.Name as ProductName
-from Product
-left join Category on Product.CategoryID = Category.PKey';
+
+// $productListQuery = 'SELECT Product.PKey as ProductPKey, Product.CreatedDate as ProductCreatedDate,
+// Category.NameKR as CategoryName, Product.Name as ProductName
+// from Product
+// left join Category on Product.CategoryID = Category.PKey';
+
 // $strAndFlag = 0;
+
 
 if(isset($_GET['productName']) || isset($_GET['categoryPKey'])){
     if(isset($_GET['productName'])) {
@@ -49,14 +52,43 @@ else{
     // left join Category on Product.CategoryID = Category.PKey
     // where Product.UserID = '.$userID.'
     // order by Product.PKey desc;';
-    $i = (int)($_POST['pageNo'])*10;
-    $productListQuery =
-    'SELECT Product.CreatedDate as ProductCreatedDate,
-    Category.NameKR as CategoryName, Product.PKey as ProductPKey, Product.Name as ProductName
-    from Product
-    left join Category on Product.CategoryID = Category.PKey
-    order by Product.PKey desc limit '.$i.', 10;';
-    // where Product.CreatedDate > "2017-09-26"
+
+  $sortMethod;
+
+  $i = (int)($_POST['pageNo'])*10;
+  $productListQuery =
+  "SELECT Product.CreatedDate as ProductCreatedDate,
+  Category.NameKR as CategoryName, Product.PKey as ProductPKey, Product.Name as ProductName
+  from Product
+  left join Category on Product.CategoryID = Category.PKey";
+  // order by Product.PKey desc limit $i, 10";
+  // if(isset($productListQuery) ) {
+  //   $productListQuery .=" order by Product.Pkey limit $i, 10";
+  // }
+  if(isset($_POST['regisDate'])){
+    if(($_POST['regisDate'])==0){
+      $productListQuery .= " order by Product.CreatedDate";
+    }
+    else{
+      $productListQuery .= " order by Product.CreatedDate desc";
+    }
+  }
+  $productListQuery .= " limit $i, 10";
+
+
+  //   if(isset($_POST['regisDate']) ){
+  //
+  //
+  //   }
+    // if(isset($_POST['categoryPKey'])){
+    //   $productListQuery .=" where Product.CategoryID = $_POST['categoryPKey']";
+    // }
+    // if(isset($_POST['regisDate']) ){
+    //   $productListQuery .=" order by date";
+    // }
+
+  // }
+
 }
 // echo $productListQuery;
 if($productTable = mysqli_query($conn, $productListQuery)){
